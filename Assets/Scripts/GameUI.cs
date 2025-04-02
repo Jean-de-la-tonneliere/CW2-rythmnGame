dec4b4eba218df97dbf00f021254cd12b2b22dca
Jean-de-la-tonneliere <jep4000@hw.ac.uk>
2025-04-02 22:48:59 +0100

    Updating error

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -5,20 +5,24 @@ using TMPro;
 public class GameUI : MonoBehaviour
 {
     [SerializeField] PlayerController Player;
+    [SerializeField] private GameObject targetEnd;  //add target in inspector 
     [SerializeField] private GameObject deathUI;
+    [SerializeField] private GameObject endUI;
     [SerializeField] public GameObject player;
-    [SerializeField] private TextMeshProUGUI livesCount;
+    [SerializeField] private TextMeshProUGUI livesCountInGame;
+    [SerializeField] private TextMeshProUGUI livesCountEndLevel;
+
 
     private void Start()
     {
         deathUI.SetActive(false);
-        livesCount.text = ":X" + (Player.getLivesCount() - 1);
+        livesCountInGame.text = ":X" + (Player.getLivesCount() - 1);
     }
 
     void Update()
     {
 
-        UpdateLivesCount();
+        UpdatelivesCountInGame();
         if (Player.getLivesCount() == 0)
         {
             //Debug.Log("Go back to menu");
@@ -33,8 +37,26 @@ public class GameUI : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); //Change scene as set in the build
     }
 
-    public void UpdateLivesCount()
+    public void GoToNextLevel()
     {
-        livesCount.text = ":X" + (Player.getLivesCount() - 1);
+        if((SceneManager.GetActiveScene().buildIndex + 1) <= SceneManager.sceneCount)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void UpdatelivesCountInGame()
+    {
+        livesCountInGame.text = ":X" + (Player.getLivesCount() - 1);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        livesCountEndLevel.text = livesCountInGame.text;
+        if (other.CompareTag("Player"))     //Set tag and collider in scene
+        {
+            endUI.SetActive(true);
+            player.SetActive(false);
+        }
     }
 }
